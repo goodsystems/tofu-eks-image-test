ea692748ced81b0d87073214d0795545b1fd0baa
Zuzana Radicova <zuzana@goodmirek.com>
2025-05-06 10:11:51 +0200

    Update providers.tf

--- tofu/providers.tf
+++ tofu/providers.tf
@@ -22,14 +22,20 @@ provider "aws" {
   }
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token # Replace exec block with this
+}
+
 provider "helm" {
   kubernetes {
     host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.cluster_name]
-    }
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
--- tofu/vars_production.tfvars
+++ tofu/vars_production.tfvars
@@ -1,8 +1,2 @@
 environment_name        = "production"
 environment_slug        = "prd"
-name                    = "Production"
-cidr_block              = "172.31.0.0/16"
-azs                     = ["us-east-1a", "us-east-1b", "us-east-1d"]
-public_cidrs            = ["172.31.224.0/24", "172.31.225.0/24", "172.31.226.0/24"]
-map_public_ip_on_launch = true
-private_cidrs           = ["172.31.96.0/19", "172.31.128.0/19", "172.31.160.0/19"]
